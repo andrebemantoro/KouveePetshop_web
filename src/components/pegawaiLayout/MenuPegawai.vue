4a3a60123c6798587d7116aee004b9c574736575
andrebemantoro <38039005+andrebemantoro@users.noreply.github.com>
2020-03-13 22:10:08 +0700

    konfirmasi delete pegawai fixed with dialog,

--- src/components/pegawaiLayout/MenuPegawai.vue
+++ src/components/pegawaiLayout/MenuPegawai.vue
@@ -55,15 +55,66 @@
                 <td>{{ item.aktif }}</td>
 
                 <td>
-                  <v-btn icon color="indigo" light @click="editHandler(item)">
+                  <v-btn icon color="blue" light @click="editHandler(item)">
                     <v-icon>mdi-pencil</v-icon>
                   </v-btn>
-                  <v-btn icon color="error" light @click="deleteData(item.id_pegawai)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                  <v-btn icon color="indigo" light @click="changePassword(item)">
+            
+                  <div class="text-center">
+                    <v-btn icon color="indigo" light @click="changePassword(item)">
                     <v-icon>mdi-lock</v-icon>
                   </v-btn>
+                  </div>
+                  
+
+                    <!-- -------------------------------------------------------- -->
+                   <div class="text-center">
+                    <v-dialog
+                      v-model="pesan"
+                      width="500"
+                      
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-btn
+                          icon
+                          color="red lighten-2"
+                          dark
+                          v-on="on"
+                        >
+                        <v-icon>mdi-delete</v-icon>
+                        </v-btn>
+                      </template>
+
+                      <v-card>
+                        <v-card-title
+                          class="headline Red lighten-2"
+                          primary-title
+                        >
+                          Konfirmasi Hapus
+                        </v-card-title>
+
+                        <v-card-text>
+                          Data yang akan dihapus, Lanjutkan ?
+                        </v-card-text>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn
+                            color="primary"
+                            text
+                            @click="deleteData(item.id_pegawai), pesan=false"
+                          >
+                            Hapus
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </div>
+                  
+                  
+                  
+                  <!-- -------------------------------------------------------- -->
                 </td>
               </tr>
             </tbody>
@@ -163,28 +214,6 @@
                 ></v-text-field>
               </v-col>
             </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text  @click="resetFormPassword(), dialogPassword = false"
-            >Close</v-btn
-          >
-          <v-btn color="blue darken-1" text @click="setForm()">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="dialogPassword" persistent max-width="600px">
-      <v-card>
-        <v-card-title>
-          <v-spacer />
-          <span class="headline">Profil Pegawai</span>
-          <v-spacer />
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-           
           </v-container>
           <small>*indicates required field</small>
         </v-card-text>
