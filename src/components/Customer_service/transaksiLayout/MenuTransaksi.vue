230178b8d9cc13e90f226edb1c8b972e831ea104
andrebemantoro <38039005+andrebemantoro@users.noreply.github.com>
2020-04-27 20:42:39 +0700

    haha

--- src/components/Customer_service/transaksiLayout/MenuTransaksi.vue
+++ src/components/Customer_service/transaksiLayout/MenuTransaksi.vue
@@ -1111,16 +1111,6 @@ export default {
       text: '',
       load: false,
       form: {
-<<<<<<< HEAD
-        progress: 'sedang diproses',
-        subtotal: '',
-        diskon: '',
-        id_jenis_hewan: '',
-        created_by: sessionStorage.getItem('Nama'),
-        delete_by: sessionStorage.getItem('Nama'),
-        modified_by: sessionStorage.getItem('Nama'),
-        id_customer_service: sessionStorage.getItem('Id'),
-=======
         subtotal: "",
         diskon: "",
         id_jenis_hewan: "",
@@ -1128,7 +1118,6 @@ export default {
         delete_by: sessionStorage.getItem("Nama"),
         modified_by: sessionStorage.getItem("Nama"),
         id_customer_service: sessionStorage.getItem("Id"),
->>>>>>> de3124933a7af3e6daa720cc4f21a9236aee1462
       },
       user: new FormData(),
       user2: new FormData(),
@@ -1229,21 +1218,6 @@ export default {
     },
     addLayanan() {
       this.detilLayanans.push({
-<<<<<<< HEAD
-        id_customer_service: sessionStorage.getItem('Id'),
-        id_transaksi_layanan: '',
-        id_harga_layanan: '',
-        jumlah: '',
-        total_harga: '',
-        progress: 'sedang diproses',
-        harga: '',
-        created_at: '',
-        created_by: sessionStorage.getItem('Nama'),
-        modified_at: '',
-        modified_by: '',
-        delete_at: '',
-        delete_by: '',
-=======
         id_customer_service: sessionStorage.getItem("Id"),
         id_transaksi_layanan: "",
         id_harga_layanan: "",
@@ -1256,7 +1230,6 @@ export default {
         modified_by: "",
         delete_at: "",
         delete_by: "",
->>>>>>> de3124933a7af3e6daa720cc4f21a9236aee1462
       });
     },
 
